a3f98242bf6f5d231c66f33850f218b0942f7226
sekarasriii <sekareeesh@gmail.com>
2025-12-25 23:01:29 +0700

    menambahkan fungsi edit data siswa di repository

--- app/src/main/java/com/example/questapi_128/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/questapi_128/repositori/RepositoriDataSiswa.kt
@@ -7,6 +7,10 @@ import com.example.questapi_128.modeldata.DataSiswa
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
+    //edit: tambahkan fungsi getSatuSiswa
+    suspend fun getSatuSiswa(id: Int): DataSiswa
+    suspend fun editSatuSiswa(id:Int, dataSiswa: DataSiswa):retrofit2.Response<Void>
+
 }
 class jaringanRepositoryDataSiswa (
     private val serviceApiSiswa: ServiceApiSiswa
