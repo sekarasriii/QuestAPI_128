fec3e938101219e12ba3fca98b515f9667c8d617
sekarasriii <sekareeesh@gmail.com>
2025-12-25 21:37:11 +0700

    menambahkan scaffold dan top app bar pada halaman entry

--- app/src/main/java/com/example/questapi_128/view/HalamanEntry.kt
+++ app/src/main/java/com/example/questapi_128/view/HalamanEntry.kt
@@ -1,10 +1,16 @@
 package com.example.questapi_128.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.questapi_128.viewmodel.EntryViewModel
+import com.example.questapi_128.uicontroller.route.DestinasiEntry
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -12,4 +18,19 @@ fun EntrySiswaScreen(
     navigateBack: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
-){}
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEntry.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack,
+                scrollBehavior = scrollBehavior
+            )
+        }
+    ){}
+}
