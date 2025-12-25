ba29f69e92f932283e8902464f1e348aa12806cc
sekarasriii <sekareeesh@gmail.com>
2025-12-25 21:34:45 +0700

    membuat kerangka layar entry siswa

---
+++ app/src/main/java/com/example/questapi_128/view/HalamanEntry.kt
@@ -0,0 +1,15 @@
+package com.example.questapi_128.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questapi_128.viewmodel.EntryViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){}
