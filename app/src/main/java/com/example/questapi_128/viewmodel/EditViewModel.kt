005c05511c4933cb409f2be21d82acc60819249b
sekarasriii <sekareeesh@gmail.com>
2025-12-25 23:26:47 +0700

    menambahkan fungsi edit data siswa ke API

--- app/src/main/java/com/example/questapi_128/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_128/viewmodel/EditViewModel.kt
@@ -47,3 +47,19 @@ class EditViewModel(
                     telpon.isNotBlank()
         }
     }
+    suspend fun editSatuSiswa() {
+        if (validasiInput(uiStateSiswa.detailSiswa)) {
+            val call: Response<Void> =
+                repositoryDataSiswa.editSatuSiswa(
+                    idSiswa,
+                    uiStateSiswa.detailSiswa.toDataSiswa()
+                )
+
+            if (call.isSuccessful) {
+                println("Update Sukses : ${call.message()}")
+            } else {
+                println("Update Error : ${call.errorBody()}")
+            }
+        }
+    }
+}
