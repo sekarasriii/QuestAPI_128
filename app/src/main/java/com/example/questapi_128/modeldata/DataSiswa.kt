e852ee27dce2a11715ef1375f93826e48dec3388
sekarasriii <sekareeesh@gmail.com>
2025-12-25 20:46:27 +0700

    menambahkan ui state dan detail siswa

--- app/src/main/java/com/example/questapi_128/modeldata/DataSiswa.kt
+++ app/src/main/java/com/example/questapi_128/modeldata/DataSiswa.kt
@@ -11,3 +11,14 @@ data class DataSiswa (
     val alamat: String,
     val telpon: String
 )
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
+data class DetailSiswa (
+    val id : Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
