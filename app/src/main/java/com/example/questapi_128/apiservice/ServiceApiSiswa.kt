dc845f729e20e911118ad11ecf33de04886f6765
sekarasriii <sekareeesh@gmail.com>
2025-12-25 20:48:29 +0700

    menambahkan service api untuk ambil dan simpan data siswa

---
+++ app/src/main/java/com/example/questapi_128/apiservice/ServiceApiSiswa.kt
@@ -0,0 +1,16 @@
+package com.example.questapi_128.apiservice
+
+import com.example.questapi_128.modeldata.DataSiswa
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
+interface ServiceApiSiswa {
+    @GET("bacaTeman.php")
+    suspend fun getSiswa(): List<DataSiswa>
+
+    @POST("insertTM.php")
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
