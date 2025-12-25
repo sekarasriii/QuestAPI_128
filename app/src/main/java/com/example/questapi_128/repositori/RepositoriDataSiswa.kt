23f765c98a8129fbc10d822cf898723492997080
sekarasriii <sekareeesh@gmail.com>
2025-12-25 23:03:12 +0700

    implementasi hapus data siswa melalui API

--- app/src/main/java/com/example/questapi_128/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/questapi_128/repositori/RepositoriDataSiswa.kt
@@ -23,4 +23,6 @@ class jaringanRepositoryDataSiswa (
     override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
     override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa):retrofit2.Response<Void> =
         serviceApiSiswa.editSatuSiswa(id, dataSiswa)
+    override suspend fun hapusSatuSiswa(id: Int): retrofit2.Response<Void> =
+        serviceApiSiswa.hapusSatuSiswa(id)
 }
