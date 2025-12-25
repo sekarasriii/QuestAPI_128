d62501067d6c71b10ce128aaa7d5f23f5329b3bc
sekarasriii <sekareeesh@gmail.com>
2025-12-26 00:00:51 +0700

    menambahkan editviewmodel dgn savedstatehandle

--- app/src/main/java/com/example/questapi_128/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questapi_128/viewmodel/provider/PenyediaViewModel.kt
@@ -25,5 +25,11 @@ object PenyediaViewModel {
                 aplikasiDataSiswa().container.repositoryDataSiswa
             )
         }
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
     }
 }
