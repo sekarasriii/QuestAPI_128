c660faabdc883d71151241d2cef70f13cbe8bb77
sekarasriii <sekareeesh@gmail.com>
2025-12-25 23:46:56 +0700

    menambahkan fungsi ambil detail siswa dengan status loading dan error

--- app/src/main/java/com/example/questapi_128/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_128/viewmodel/DetailViewModel.kt
@@ -37,3 +37,18 @@ class DetailViewModel(
         getSatuSiswa()
     }
 
+    fun getSatuSiswa() {
+        viewModelScope.launch {
+            statusUIDetail = StatusUIDetail.Loading
+            statusUIDetail = try {
+                StatusUIDetail.Success(
+                    statusiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                )
+            } catch (e: IOException) {
+                StatusUIDetail.Error
+            } catch (e: HttpException) {
+                StatusUIDetail.Error
+            }
+        }
+    }
+
