de08edbe577f4f6f652a458debaf2038dc95957b
sekarasriii <sekareeesh@gmail.com>
2025-12-26 00:17:58 +0700

    menambahkan setup dasar editsiswa screen dan menambahkan parameter navigasi&modifier

--- app/src/main/java/com/example/questapi_128/view/HalamanEdit.kt
+++ app/src/main/java/com/example/questapi_128/view/HalamanEdit.kt
@@ -1,4 +1,22 @@
 package com.example.questapi_128.view
 
-class HalamanEdit {
-}
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.questapi_128.viewmodel.EditViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questapi_128.viewmodel.provider.PenyediaViewModel
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.res.stringResource
+import androidx.compose.runtime.rememberCoroutineScope
+import kotlinx.coroutines.launch
+import androidx.compose.foundation.layout.padding
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+)
