2c11117eae3456d712c6a2fe2433645729e283bb
sekarasriii <sekareeesh@gmail.com>
2025-12-25 21:14:43 +0700

    menambahkan destinasi Entry Siswa

---
+++ app/src/main/java/com/example/questapi_128/uicontroller/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.questapi_128.uicontroller.route
+
+import com.example.questapi_128.R
+
+object DestinasiEntry: DestinasiNavigasi{
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
