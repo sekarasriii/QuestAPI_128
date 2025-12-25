477b12116ea1fb81cdbcec60ecc7a2ec4dea047b
sekarasriii <sekareeesh@gmail.com>
2025-12-25 23:45:54 +0700

    mengambil id siswa dari savedstatehandle

--- app/src/main/java/com/example/questapi_128/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_128/viewmodel/DetailViewModel.kt
@@ -25,4 +25,11 @@ sealed interface StatusUIDetail {
 class DetailViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoryDataSiswa: RepositoryDataSiswa
-) : ViewModel()
+) : ViewModel(){
+
+    private val idSiswa: Int =
+        checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+
