711e9e1317bd42d4502ab7f3979e34a6978836cd
sekarasriii <sekareeesh@gmail.com>
2025-12-25 20:52:50 +0700

    menambahkan implementasi repository berbasis jaringan

--- app/src/main/java/com/example/questapi_128/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/questapi_128/repositori/RepositoriDataSiswa.kt
@@ -8,3 +8,10 @@ interface RepositoryDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
 }
+class jaringanRepositoryDataSiswa (
+    private val serviceApiSiswa: ServiceApiSiswa
+): RepositoryDataSiswa{
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2
+    .Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+}
