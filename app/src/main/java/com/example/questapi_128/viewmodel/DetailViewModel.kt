686c48e14b7d23eea25b047d1f612929fe74ca05
sekarasriii <sekareeesh@gmail.com>
2025-12-25 23:43:06 +0700

    menambahkan sealed interface status UI detail siswa

--- app/src/main/java/com/example/questapi_128/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_128/viewmodel/DetailViewModel.kt
@@ -1,4 +1,24 @@
 package com.example.questapi_128.viewmodel
 
-class DetailViewModel {
+import android.annotation.SuppressLint
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_128.modeldata.DataSiswa
+import com.example.questapi_128.repositori.RepositoryDataSiswa
+import com.example.questapi_128.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import kotlinx.serialization.InternalSerializationApi
+import retrofit2.HttpException
+import retrofit2.Response
+import java.io.IOException
+
+sealed interface StatusUIDetail {
+    data class Success(val statusiswa: DataSiswa) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
 }
+
