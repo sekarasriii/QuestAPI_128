2320392b3527d40b308a01751990a3a8b66570e5
sekarasriii <sekareeesh@gmail.com>
2025-12-25 21:14:07 +0700

    menambahkan destinasi Home sbg layar awal

---
+++ app/src/main/java/com/example/questapi_128/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.questapi_128.uicontroller.route
+
+import com.example.questapi_128.R
+
+object DestinasiHome: DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = R.string.nama
+}
